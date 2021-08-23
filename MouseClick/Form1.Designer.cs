29270a6d171e24bf4c48b5b96344f32b7ab0950d
longxing <695017153@qq.com>
2021-08-23 15:04:58 +0800

    '初始版本'
    Signed-off-by: longxing <695017153@qq.com>

---
+++ MouseClick/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MouseClick
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxInterval = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonCapPos = new System.Windows.Forms.Button();
+            this.timerGetCurpos = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelMain = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timerUpdateUi = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabelNum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(26, 39);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 21);
+            this.textBoxX.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBoxX, "横坐标");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxInterval);
+            this.groupBox1.Controls.Add(this.textBoxY);
+            this.groupBox1.Controls.Add(this.textBoxX);
+            this.groupBox1.Location = new System.Drawing.Point(36, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(435, 79);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "坐标与间隔";
+            // 
+            // textBoxInterval
+            // 
+            this.textBoxInterval.Location = new System.Drawing.Point(297, 39);
+            this.textBoxInterval.Name = "textBoxInterval";
+            this.textBoxInterval.Size = new System.Drawing.Size(100, 21);
+            this.textBoxInterval.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBoxInterval, "时间间隔(s)");
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(164, 39);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 21);
+            this.textBoxY.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.textBoxY, "纵坐标");
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(106, 179);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(265, 179);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "结束";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonCapPos
+            // 
+            this.buttonCapPos.Location = new System.Drawing.Point(188, 130);
+            this.buttonCapPos.Name = "buttonCapPos";
+            this.buttonCapPos.Size = new System.Drawing.Size(75, 23);
+            this.buttonCapPos.TabIndex = 4;
+            this.buttonCapPos.Text = "获取坐标点";
+            this.buttonCapPos.UseVisualStyleBackColor = true;
+            this.buttonCapPos.Click += new System.EventHandler(this.buttonCapPos_Click);
+            // 
+            // timerGetCurpos
+            // 
+            this.timerGetCurpos.Tick += new System.EventHandler(this.timerGetCurpos_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelNum,
+            this.toolStripStatusLabelMain});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 359);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(497, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelMain
+            // 
+            this.toolStripStatusLabelMain.Name = "toolStripStatusLabelMain";
+            this.toolStripStatusLabelMain.Size = new System.Drawing.Size(32, 17);
+            this.toolStripStatusLabelMain.Text = "就绪";
+            // 
+            // timerUpdateUi
+            // 
+            this.timerUpdateUi.Tick += new System.EventHandler(this.timerUpdateUi_Tick);
+            // 
+            // toolStripStatusLabelNum
+            // 
+            this.toolStripStatusLabelNum.Name = "toolStripStatusLabelNum";
+            this.toolStripStatusLabelNum.Size = new System.Drawing.Size(20, 17);
+            this.toolStripStatusLabelNum.Text = "   ";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 381);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonCapPos);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
+            this.Name = "FormMain";
+            this.Text = "鼠标定时点击程序v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBoxInterval;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonCapPos;
+        private System.Windows.Forms.Timer timerGetCurpos;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelMain;
+        private System.Windows.Forms.Timer timerUpdateUi;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelNum;
+    }
+}
+
---
+++ MouseClick/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace MouseClick
+{
+    public partial class FormMain : Form
+    {
+        OperaThread m_operaThread = null;
+        Thread m_threadOpera = null;
+        private int m_num = 0;
+        private int m_interval = 0;
+        //private volatile bool m_isesc = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初始化
+            textBoxX.Text = 1130.ToString();
+            textBoxY.Text = 600.ToString();
+            textBoxInterval.Text = (30 * 60 + 100).ToString();   // 15分钟加上20秒
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                timerGetCurpos.Enabled = false;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            try
+            {
+
+                x = Convert.ToInt32(textBoxX.Text);
+                y = Convert.ToInt32(textBoxY.Text);
+                m_interval = Convert.ToInt32(textBoxInterval.Text);
+
+                int[] params1 = new int[] { x, y, m_interval };
+                if (m_operaThread == null)
+                {
+                    m_operaThread = new OperaThread();
+                    //ThreadStart thStart=new ThreadStart(m_operaThread.RunLoop);
+                    //m_operaThread.is
+                }
+                if (m_threadOpera == null)
+                {
+                    m_threadOpera = new Thread(m_operaThread.RunLoop);
+                }
+                m_threadOpera.Name = "鼠标操作线程";
+                m_threadOpera.IsBackground = true;
+                m_threadOpera.Start(params1);
+
+                timerUpdateUi.Interval = 2000;
+                timerUpdateUi.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+        // 获取坐标点
+        private void buttonCapPos_Click(object sender, EventArgs e)
+        {
+            timerGetCurpos.Interval = 1000;
+            timerGetCurpos.Enabled = true;
+        }
+
+        private void timerGetCurpos_Tick(object sender, EventArgs e)
+        {
+            textBoxX.Text = Cursor.Position.X.ToString();
+            textBoxY.Text = Cursor.Position.Y.ToString();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timerUpdateUi.Enabled = false;
+
+            if (m_operaThread != null)
+            {
+                m_operaThread.StopThread();
+            }
+            m_threadOpera = null;
+            m_num = 0;
+
+            toolStripStatusLabelMain.Text = "就绪";
+        }
+
+        private void timerUpdateUi_Tick(object sender, EventArgs e)
+        {
+            if (m_threadOpera != null && m_threadOpera.IsAlive)
+            {
+                if (m_num != m_operaThread.m_num)
+                {
+                    toolStripStatusLabelNum.Text = m_operaThread.m_num + " -";
+                    m_num = m_operaThread.m_num;
+                }
+                else if (m_operaThread.m_timenum > 0)
+                {
+                    toolStripStatusLabelMain.Text = "距离下一次点击还有" + (m_interval - m_operaThread.m_timenum) + "秒";
+                }
+            }
+        }
+
+    }
+
+    class OperaThread
+    {
+        private volatile bool m_isRun = false;
+        public volatile int m_num = 0;
+        public volatile int m_timenum = 0;
+
+        public enum MouseEventFlags
+        {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            MiddleDown = 0x0020,
+            MiddleUp = 0x0040,
+            Wheel = 0x0800,
+            Absolute = 0x8000
+        }
+        [DllImport("User32")]
+        public extern static void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
+        /// <summary>
+        /// 引用user32.dll动态链接库（windows api），
+        /// 使用库中定义 API：SetCursorPos
+        /// </summary>
+        [DllImport("user32.dll")]
+        private static extern int SetCursorPos(int x, int y);
+        /// <summary>
+        /// 移动鼠标到指定的坐标点
+        /// </summary>
+        public void MoveMouseToPoint(Point p)
+        {
+            SetCursorPos(p.X, p.Y);
+        }
+        /// <summary>
+        /// 设置鼠标的移动范围
+        /// </summary>
+        public void SetMouseRectangle(Rectangle rectangle)
+        {
+            System.Windows.Forms.Cursor.Clip = rectangle;
+        }
+        /// <summary>
+        /// 设置鼠标位于屏幕中心
+        /// </summary>
+        public void SetMouseAtCenterScreen()
+        {
+            int winHeight = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+            int winWidth = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width;
+            Point centerP = new Point(0, 0);
+            MoveMouseToPoint(centerP);
+        }
+
+
+        public void RunLoop(object param1)
+        {
+            int x, y, interval;
+            int[] tempInts;
+            Point curPos = new Point();
+            //int m_timenum = 0;
+
+            if (!(param1 is int[]))
+            {
+                Console.WriteLine("参数类型错误");
+                return;
+            }
+            tempInts = param1 as int[];
+            if (tempInts.Length != 3)
+            {
+                Console.WriteLine("参数数量错误");
+                return;
+            }
+            x = tempInts[0];
+            y = tempInts[1];
+            interval = tempInts[2];
+            m_isRun = true;
+            m_num = 0;
+            m_timenum = 0;
+            curPos.X = x;
+            curPos.Y = y;
+
+            do
+            {
+                ++m_num;
+                Console.WriteLine("第" + m_num + "次鼠标点击");
+                MoveMouseToPoint(curPos);
+                Thread.Sleep(500);
+                mouse_event((int)(MouseEventFlags.LeftDown | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+                //Thread.Sleep(500);
+                mouse_event((int)(MouseEventFlags.LeftUp | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+#if false
+                Thread.Sleep(2000);
+                mouse_event((int)(MouseEventFlags.LeftDown | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+                //Thread.Sleep(500);
+                mouse_event((int)(MouseEventFlags.LeftUp | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+#endif
+                // 等待一段时间
+                while (m_timenum < interval)
+                {
+                    if (!m_isRun)
+                    {
+                        break;
+                    }
+                    // 一次两秒
+                    m_timenum += 2;
+                    Thread.Sleep(2000);
+                }
+                m_timenum = 0;
+                //Thread.Sleep(interval * 1000);
+            } while (m_isRun);
+
+            //m_threadOpera = null;
+            Console.WriteLine("鼠标定位点击线程结束");
+        }
+
+        public void StopThread()
+        {
+            m_isRun = false;
+        }
+    }
+
+
+}
